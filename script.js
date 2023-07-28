9ac08a9f96085fa32b0dbcf9c21656b60330df3c
Naushad <naushadlil01@gmail.com>
2023-07-28 10:29:33 +0530

    fixed path and added event

--- script.js
+++ script.js
@@ -2,19 +2,19 @@ console.log("Welcome to Spotify");
 
 // initialize variables
 let songIndex = 0;
-let audioElement = new Audio("/Projec-1/songs/1.mp3");
+let audioElement = new Audio("/songs/1.mp3");
 let masterPlay = document.getElementById('masterPlay');
 let myprogressbar = document.getElementById('myprogressbar');
 let gif = document.getElementById('gif');
 let songItems = Array.from(document.getElementsByClassName('songitem'));
 let songs = [
-    {songName : "Warriyo - Mortals (feat.  Laura Brehm) [NCS Release]", filePath : "/Projec-1/songs/1.mp3", coverPath : "/Projec-1/covers/1.jpg"},
-    {songName : "Cialo - Huma-Huma", filePath : "/Projec-1/songs/2.mp3", coverPath : "/Projec-1/covers/2.jpg"},
-    {songName : "DEAF KEV - Invincible [NCS Release]-320k", filePath : "/Projec-1/songs/3.mp3", coverPath : "/Projec-1/covers/3.jpg"},
-    {songName : "Rabba - Salam-e-Ishq", filePath : "/Projec-1/songs/4.mp3", coverPath : "/Projec-1/covers/4.jpg"},
-    {songName : "Sakhiyaan - Salam-e-Ishq", filePath : "/Projec-1/songs/5.mp3", coverPath : "/Projec-1/covers/5.jpg"},
-    {songName : "Bhula Dena - Salam-e-Ishq", filePath : "/Projec-1/songs/6.mp3", coverPath : "/Projec-1/covers/6.jpg"},
-    {songName : "Tumhari kasam - Salam-e-Ishq", filePath : "/Projec-1/songs/7.mp3", coverPath : "/Projec-1/covers/7.jpg"}
+    {songName : "Warriyo - Mortals (feat. Laura Brehm)", filePath : "/songs/1.mp3", coverPath : "/covers/1.jpg"},
+    {songName : "Cialo - Huma-Huma", filePath : "/songs/2.mp3", coverPath : "/covers/2.jpg"},
+    {songName : "DEAF KEV - Invincible [NCS Release]-320k", filePath : "/songs/3.mp3", coverPath : "/covers/3.jpg"},
+    {songName : "Rabba - Salam-e-Ishq", filePath : "/songs/4.mp3", coverPath : "/covers/4.jpg"},
+    {songName : "Sakhiyaan - Salam-e-Ishq", filePath : "/songs/5.mp3", coverPath : "/covers/5.jpg"},
+    {songName : "Bhula Dena - Salam-e-Ishq", filePath : "/songs/6.mp3", coverPath : "/covers/6.jpg"},
+    {songName : "Tumhari kasam - Salam-e-Ishq", filePath : "/songs/7.mp3", coverPath : "/covers/7.jpg"}
 ]
 
 // songItem list src update
@@ -24,6 +24,7 @@ songItems.forEach((element,i)=>{
 });
 
 // Handle master-play/pause  click
+
 masterPlay.addEventListener('click', ()=>{
     if(audioElement.paused || audioElement.currentTime<=0){
         audioElement.play();
@@ -48,7 +49,7 @@ document.getElementById('previous').addEventListener('click', ()=>{
         songIndex -=1;
     }
     audioElement.currentTime = 0;
-    audioElement.src = `/Projec-1/songs/${songIndex+1}.mp3`;
+    audioElement.src = `/songs/${songIndex+1}.mp3`;
     audioElement.play();
     masterPlay.classList.remove('fa-circle-play');
     masterPlay.classList.add('fa-circle-pause');
@@ -63,7 +64,7 @@ document.getElementById('next').addEventListener('click', ()=>{
         songIndex +=1;
     }
     audioElement.currentTime = 0;
-    audioElement.src = `/Projec-1/songs/${songIndex+1}.mp3`;
+    audioElement.src = `/songs/${songIndex+1}.mp3`;
     audioElement.play();
     masterPlay.classList.remove('fa-circle-play');
     masterPlay.classList.add('fa-circle-pause');
@@ -91,18 +92,39 @@ const makeAllPlays = ()=>{
     })
 }
 
+const makeAllPause = ()=>{
+    Array.from(document.getElementsByClassName('songItemPlay')).forEach((element)=>{
+        element.classList.remove('fa-circle-play');
+        element.classList.add('fa-circle-pause');
+    })
+}
+
 Array.from(document.getElementsByClassName('songItemPlay')).forEach((element)=>{
      element.addEventListener('click', (e)=>{
+        if(audioElement.paused || audioElement.currentTime<=0){
         makeAllPlays();
         songIndex = parseInt(e.target.id);
         e.target.classList.remove('fa-circle-play');
         e.target.classList.add('fa-circle-pause');                   
         audioElement.currentTime = 0;
-        audioElement.src = `/Projec-1/songs/${songIndex+1}.mp3`;
+        audioElement.src = `/songs/${songIndex+1}.mp3`;
         audioElement.play();
         masterPlay.classList.remove('fa-circle-play');
         masterPlay.classList.add('fa-circle-pause');
         gif.style.opacity = 1;
+        }
+        else{
+            // makeAllPause();
+            songIndex = parseInt(e.target.id);
+            e.target.classList.remove('fa-circle-pause');
+            e.target.classList.add('fa-circle-play');                   
+            audioElement.currentTime = 0;
+            audioElement.src = `/songs/${songIndex+1}.mp3`;
+            audioElement.pause();
+            masterPlay.classList.remove('fa-circle-pause');
+            masterPlay.classList.add('fa-circle-play');
+            gif.style.opacity = 0;    
+        }
      })
 })
 
